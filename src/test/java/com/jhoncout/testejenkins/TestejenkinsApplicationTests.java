783e96db51061f5349f33fa9c7a4dfdfedc5e550
JonathanCout <jqcoutinho.eng@gmail.com>
2021-11-18 11:11:50 -0300

    iniciando jenkins

---
+++ src/test/java/com/jhoncout/testejenkins/TestejenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhoncout.testejenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestejenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
